8cb589e9190234afe80cd3da9aa72ea10dd6281a
sagar <senjaliyasagar2002@gmail.com>
2022-11-07 11:30:39 +0530

    final

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports ={
+    MONGO_URI:"mongodb+srv://sagar:sagar@cluster0.jje5vo5.mongodb.net/pre_school?retryWrites=true&w=majority",
+    JWT_SECRET:"MOHENMONDALJDFKFLL"
+
+}
+//mongodb+srv://sagar:sagar@cluster0.jje5vo5.mongodb.net/pre_school?retryWrites=true&w=majority
+
+
+
+
+// mongodb+srv://mahen:12345a@cluster0.qfgbe.mongodb.net/E-voting?retryWrites=true&w=majority
